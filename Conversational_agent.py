03ab3473e1a15ea0d0be8565d88735725ea52274
Pedro Frau <pedro.frauamar@gmail.com>
2023-02-03 16:10:04 +0100

    Sending data to whisper after X seconds or after Y seconds of silence

--- Conversational_agent.py
+++ Conversational_agent.py
@@ -1,20 +1,20 @@
 import sys
 import asyncio
-import live_transcriber as lt
+import ASR_module as asr
 import chatgpt_bridge as gpt
 
 
 async def main():
     print('\nStarting listener\n')
-    listener_task = asyncio.create_task(lt.process_input_audio())
+    listener_task = asyncio.create_task(asr.process_input_audio())
 
     while True:
         await asyncio.sleep(1)
-        trans_res = lt.transcription_result
+        trans_res = asr.transcription_result
         if trans_res != "":
             print(trans_res)
             gpt.process_request(trans_res)
-            lt.set_transcription_result("")
+            asr.set_transcription_result("")
 
     listener_task.cancel()
 
